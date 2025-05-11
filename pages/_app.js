1147fe83c17fdea5aa28caa0fddf5b20ede1c891
Benard <benard518@gmail.com>
2025-05-11 16:07:26 +0300

    updating components exports and cleanup

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "@/styles/globals.css";
 
+// INTERNAL IMPORT
+import { Footer, NavBar } from "@/components/componentsindex";
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <NavBar />
+      <Component {...pageProps} />
+      <Footer />
+    </div>
+  )
 }
