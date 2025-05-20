5c1225d9d416a98c626849584d26b7b6c00d9ac1
ben <benardoochanda@gmail.com>
2025-05-20 12:35:04 +0300

    adding the nfttdetails page and components

---
+++ NFTDetailsPage/NFTDescription/NFTDescription.jsx
@@ -0,0 +1,295 @@
+import React, { useState } from "react";
+import Image from "next/image";
+import {
+  MdVerified,
+  MdCloudUpload,
+  MdTimer,
+  MdReportProblem,
+  MdDeleteSweep,
+  MdOutlineDeleteSweep,
+} from "react-icons/md";
+import { BsThreeDots } from "react-icons/bs";
+import { FaWallet, FaPercentage } from "react-icons/fa";
+import {
+  TiSocialFacebook,
+  TiSocialLinkedin,
+  TiSocialTwitter,
+  TiSocialYoutube,
+  TiSocialInstagram,
+} from "react-icons/ti";
+import { BiTransferAlt, BiDollar } from "react-icons/bi";
+
+// INTERNAL IMPORT
+import Style from "./NFTDescription.module.css";
+import images from "../../img";
+import { NFTTabs } from "../NFTDetailsIndex";
+import { Button } from "@/components/componentsindex";
+
+const NFTDescription = () => {
+  const [social, setSocial] = useState(false);
+  const [NFTMenu, setNFTMenu] = useState(false);
+  const [history, setHistory] = useState(true);
+  const [provenance, setProvenance] = useState(false);
+  const [owner, setOwner] = useState(false);
+
+  const historyArray = [
+    images.user1,
+    images.user2,
+    images.user3,
+    images.user4,
+    images.user5,
+    images.user1,
+    images.user2,
+  ];
+
+  const provenanceArray = [
+    images.user1,
+    images.user5,
+    images.user3,
+    images.user4,
+    images.user2,
+  ];
+
+  const ownerArray = [
+    images.user5,
+    images.user1,
+    images.user4,
+    images.user2,
+    images.user3,
+    images.user5,
+  ];
+
+  const openSocial = () => {
+    if (!social) {
+      setSocial(true);
+      setNFTMenu(false);
+    } else {
+      setSocial(false);
+    }
+  };
+
+  const openNFTMenu = () => {
+    if (!NFTMenu) {
+      setNFTMenu(true);
+      setSocial(false);
+    } else {
+      setNFTMenu(false);
+    }
+  };
+
+  const openTabs = (e) => {
+    const btnText = e.target.innerText;
+    if (btnText === "Bid History") {
+      setHistory(true);
+      setProvenance(false);
+      setOwner(false);
+    } else if (btnText === "Provenance") {
+      setHistory(false);
+      setProvenance(true);
+      setOwner(false);
+    }
+  };
+
+  const openOwner = () => {
+    if (!owner) {
+      setOwner(true);
+      setHistory(false);
+      setProvenance(false);
+    } else {
+      setOwner(false);
+      setHistory(true);
+    }
+  };
+  return (
+    <div className={Style.NFTDescription}>
+      <div className={Style.NFTDescription_box}>
+        {/* PART ONE */}
+        <div className={Style.NFTDescription_box_share}>
+          <p>Virtual Worlds</p>
+          <div className={Style.NFTDescription_box_share_box}>
+            <MdCloudUpload
+              className={Style.NFTDescription_box_share_box_icon}
+              onClick={() => openSocial()}
+            />
+            {social && (
+              <div className={Style.NFTDescription_box_share_box_social}>
+                <a href="#">
+                  <TiSocialFacebook /> Facebook
+                </a>
+                <a href="#">
+                  <TiSocialInstagram /> Instagram
+                </a>
+                <a href="#">
+                  <TiSocialLinkedin /> LinkedIn
+                </a>
+                <a href="#">
+                  <TiSocialTwitter /> Twitter
+                </a>
+                <a href="#">
+                  <TiSocialYoutube /> Youtube
+                </a>
+              </div>
+            )}
+
+            <BsThreeDots
+              className={Style.NFTDescription_box_share_box_icon}
+              onClick={() => openNFTMenu()}
+            />
+
+            {NFTMenu && (
+              <div className={Style.NFTDescription_box_share_box_social}>
+                <a href="#">
+                  <BiDollar /> Change Price
+                </a>
+                <a href="#">
+                  <BiTransferAlt /> Transfer Token
+                </a>
+                <a href="#">
+                  <MdReportProblem /> Report Abuse
+                </a>
+                <a href="#">
+                  <MdOutlineDeleteSweep /> Delete item
+                </a>
+              </div>
+            )}
+          </div>
+        </div>
+
+        {/* PART TWO */}
+        <div className={Style.NFTDescription_box_profile}>
+          <h1>BearX #32454</h1>
+          <div className={Style.NFTDescription_box_profile_box}>
+            <div className={Style.NFTDescription_box_profile_box_left}>
+              <Image
+                src={images.user1}
+                alt="profile"
+                width={40}
+                height={40}
+                className={Style.NFTDescription_box_profile_box_left_img}
+              />
+              <div className={Style.NFTDescription_box_profile_box_left_info}>
+                <small>Creator</small> <br />
+                <span>
+                  Karli Costa <MdVerified />
+                </span>
+              </div>
+            </div>
+
+            <div className={Style.NFTDescription_box_profile_box_right}>
+              <Image
+                src={images.user2}
+                alt="profile"
+                width={40}
+                height={40}
+                className={Style.NFTDescription_box_profile_box_left_img}
+              />
+
+              <div className={Style.NFTDescription_box_profile_box_right_info}>
+                <small>Creator</small> <br />
+                <span>
+                  Karli Costa <MdVerified />
+                </span>
+              </div>
+            </div>
+          </div>
+
+          <div className={Style.NFTDescription_box_profile_biding}>
+            <p>
+              <MdTimer /> <span>Auction Ending In:</span>
+            </p>
+            <div className={Style.NFTDescription_box_profile_biding_box_timer}>
+              <div
+                className={
+                  Style.NFTDescription_box_profile_biding_box_timer_item
+                }
+              >
+                <p>2</p>
+                <span>Days</span>
+              </div>
+              <div
+                className={
+                  Style.NFTDescription_box_profile_biding_box_timer_item
+                }
+              >
+                <p>22</p>
+                <span>hours</span>
+              </div>
+              <div
+                className={
+                  Style.NFTDescription_box_profile_biding_box_timer_item
+                }
+              >
+                <p>45</p>
+                <span>mins</span>
+              </div>
+              <div
+                className={
+                  Style.NFTDescription_box_profile_biding_box_timer_item
+                }
+              >
+                <p>22</p>
+                <span>secs</span>
+              </div>
+            </div>
+
+            <div className={Style.NFTDescription_box_profile_biding_box_price}>
+              <div
+                className={
+                  Style.NFTDescription_box_profile_biding_box_price_bid
+                }
+              >
+                <small>Current Bid</small>
+                <p>
+                  1.000 ETH <span>(~$3,200.22)</span>
+                </p>
+              </div>
+
+              <span>[96 in stock]</span>
+            </div>
+
+            <div className={Style.NFTDescription_box_profile_biding_box_button}>
+              <Button
+                icon={<FaWallet />}
+                btnName="Place a Bid"
+                handleClick={() => {}}
+                classStyle={Style.button}
+              />
+              <Button
+                icon={<FaPercentage />}
+                btnName="Make Offer"
+                handleClick={() => {}}
+                classStyle={Style.button}
+              />
+            </div>
+
+            <div className={Style.NFTDescription_box_profile_biding_box_tabs}>
+              <button onClick={(e) => openTabs(e)}>Bid History</button>
+              <button onClick={(e) => openTabs(e)}>Provenance</button>
+              <button onClick={(e) => openOwner()}>Owner</button>
+            </div>
+
+            {history && (
+              <div className={Style.NFTDescription_box_profile_biding_box_card}>
+                <NFTTabs dataTab={historyArray} />
+              </div>
+            )}
+
+            {provenance && (
+              <div className={Style.NFTDescription_box_profile_biding_box_card}>
+                <NFTTabs dataTab={provenanceArray} />
+              </div>
+            )}
+
+            {owner && (
+              <div className={Style.NFTDescription_box_profile_biding_box_card}>
+                <NFTTabs dataTab={ownerArray} icon={<MdVerified />} />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NFTDescription;
